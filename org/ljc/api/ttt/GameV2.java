5cb170310d87a9cfb082bffcfc970ae955bf9146
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-07-15 12:14:03 +0100

    Fix formatting

--- org/ljc/api/ttt/GameV2.java
+++ org/ljc/api/ttt/GameV2.java
@@ -2,5 +2,7 @@ package org.ljc.api.ttt;
 
 public interface GameV2 {
     Player get();
-    GameV2 Play(Player player, Position position);
+    GameV2 Play(
+        Player player,
+        Position position);
 }
